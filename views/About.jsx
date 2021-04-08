ea4b3191a90d69b77114c62ca96caf825d726c92
GDusek <70817559+GDusek@users.noreply.github.com>
2021-04-08 14:19:01 -0500

    setup About route component

---
+++ views/About.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const Layout = require('./Components/Layout.jsx');
+
+class About extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <h2>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam reiciendis aperiam ad officia obcaecati aliquid assumenda ullam, beatae maiores quo culpa error neque similique dolore placeat nobis ut. Aliquam, non!
+                </h2>
+            </Layout>
+        )
+    }
+}
+module.exports = About;
